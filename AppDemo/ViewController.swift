2318b6a16a3fd0b6a58aab5e025918c80c7bcade
AdministradorUTM <administradorutm@iMac-de-AdministradorUTM.local>
2016-12-06 09:57:11 -0600

    nueva version de la ventas

--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,
+UITableViewDelegate{
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Vista cargada")
     }
+    @IBOutlet weak var buton: UIButton!
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 10
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        
+        let proto = (indexPath.row % 2 == 0 ) ? "proto1" : "proto2"
+        let vista = tableView.dequeueReusableCell(withIdentifier: proto, for: indexPath) as! ilaTableViewCell
+        
+        if indexPath.row % 2 == 0{
+        vista.lbIzquierda.text = "Index!"
+        vista.lbDerecha.text = "\(indexPath.row)"
+        
+        } else {
+        vista.lbDerecha.text = "\(indexPath.row)"
+        vista.lbIzquierda.text = "Index!"
+        
+        }
+        return vista
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Fila \(indexPath.row)")
+    }
+    
 }
 
---
+++ ilaTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ilaTableViewCell.swift
+//  AppDemo
+//
+//  Created by AdministradorUTM on 05/12/16.
+//  Copyright © 2016 CursodeIOs. All rights reserved.
+//
+
+import UIKit
+
+class ilaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbIzquierda: UILabel!
+    
+    
+    @IBOutlet weak var lbDerecha: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
